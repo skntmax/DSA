97a99a7e8014b442faa3ee8ba091a24b79f7a867
Shashi Kant <skntjee@gmail.com>
2022-09-13 18:07:29 +0530

    check pelindrome for -v and + integer

--- pelindrome.js
+++ pelindrome.js
@@ -19,3 +19,41 @@ let str =  "dadsdd"
  
 
   
+ 
+ // check pelidnrom for -v and +ve integer as well 
+ 
+ 
+ 
+var isPalindrome = function(x) {
+     
+    if(x<0) {
+        let rv  ="" 
+         let v= x.toString() 
+          for(let i=x.toString().length-1; i>0 ; i--) {
+            rv+=x.toString()[i]
+          }
+         rv =rv+v[0]
+        if(v==rv.toString() ) return true  
+        else return false  
+           
+    }else{
+         let rv = ""
+         let v = x 
+           for(let i=x.toString().length-1; i>=0 ; i--   ) {
+                rv+=x.toString()[i]
+           }
+        //   let rV = x.toString().split('').sort((a,b)=>b-a )  
+          if(v==rv) return true 
+          else return false
+        
+    }
+         
+    
+    };
+
+
+
+    console.log(isPalindrome(-121)) 
+ 
+
+  
