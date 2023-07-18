394d26d6e1214eb5230867d8deee608538942e0d
Shashi Kant <skntjee@gmail.com>
2023-07-18 17:42:31 +0530

    map and filter polyfills

--- filter_polyfill.js
+++ filter_polyfill.js
@@ -1,19 +1,31 @@
   
- let arr = [1,2,4,5] 
+ let arr = [
+    {name:"skntmax" ,addres:"kapur" } , 
+    {name:"skntmax1" ,addres:"kapur1" } , 
+    {name:"skntmax2" ,addres:"kapur2" } , 
+    {name:"skntmax3" ,addres:"kapur2" } , 
+] 
   
-
 
 Array.prototype.myFilter = function(cb){
      let res = []
       for(let i=0 ; i<this.length; i++ ) {
           if(cb(this[i], i , this )) {
-             res.push(cb(this[i], i , this ))
-          } 
+            res.push(this[i])           
+           } 
+       }
+       return res 
+  }
 
-      }
+  console.log(arr.myFilter((ele)=> ele.name=="skntmax" ));
 
-      return res 
-      
-}
 
-  console.log(arr.filter((ele)=>ele>2  ));
+console.log(arr.filter((ele)=> {
+    if(ele.name=="skntmax") {
+      return { md :"modified " , ...ele  }
+    }
+  
+} ));
+
+
+
