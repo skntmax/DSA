dc05de9e3402c15b499d1f2b6384a8b6d2f6ec29
Shashi Kant <skntjee@gmail.com>
2023-07-18 12:31:11 +0530

    this keyword

--- MyMap.js
+++ MyMap.js
@@ -1,18 +1,18 @@
 Array.prototype.myMap = function (cb) {
         let result = []
 
-       for( let i=0 ; i<this.length ; i++) {
+       for( let i=0 ;  i<this.length ; i++) {
            let counter = cb(this[i] , i , this)
            result.push(counter)        
-       }
-
+         }
+        
        return result 
-      
 }
-
 
 let arr= [1,2,4,5,6,7]
 
  console.log(arr.myMap((ele , index , arrray )=> {
     return ele*2
  } )) 
+
+ 
