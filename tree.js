533d2cad547936ac42d01c7a87e3aefb078be0ab
Shashi Kant <skntjee@gmail.com>
2023-07-18 13:35:25 +0530

    tree insertion and serch

--- tree.js
+++ tree.js
@@ -20,6 +20,61 @@ class tree {
              }
         }
 
+          searchNode(value) {
+             if(this.isTreeEmpty()) {
+                return "tree is empy"
+             }
+            if(this.root.val==value){
+                 return true 
+            }
+            else{
+                    let status = this.searchToEveryNode(this.root , value )
+                     return this.searchToEveryNode(this.root , value )
+              } 
+          
+        }
+
+           
+          searchToEveryNode( root , value ){
+           
+            if(root.val>value)  {
+                if(root.val==value) {
+                   return true 
+                }else if(root.left==null){
+                     return false    
+                }else{
+                     
+                    return this.searchToEveryNode(root.left , value )   
+                }
+             }
+              
+              else if(root.val<value)  {
+                if(root.val==value) {
+                   return true 
+                }else if(root.right==null){
+                    return false 
+               }else{
+                   return this.searchToEveryNode(root.right , value ) 
+               }
+              
+              
+             }
+              
+             else if(root.val==value)  {
+               return true 
+             }
+
+                 
+            
+
+              
+
+          }
+
+
+
+
+
         insertSingleNode(root, new_node){
             if(root.val>new_node.val) {
                    if(root.left==null){
@@ -54,4 +109,5 @@ obj.insertNode(293)
 obj.insertNode(653)
 obj.insertNode(21762572)
 obj.insertNode(99879)
-console.log(obj);
+
+console.log(obj.searchNode(67565));
