d2d444938b2226fb3128b7c3f71638561f4b3df8
Shashi Kant <skntjee@gmail.com>
2023-09-24 13:12:04 +0530

    prototypes

--- first_class_function.js
+++ first_class_function.js
@@ -1,13 +1,47 @@
-function fcf() {
-      console.log("callback ")
-}
 
-function root(fcf) {
-   
-     console.log("some asynchronous work here ")
-      fcf()
-     
-}
+ Array.prototype.removeDupulicte = function() {
+    return this.filter((ele, ind )=>this.indexOf(ele)==ind)    
+} //  prototype for removeDuplicate 
 
 
-root(fcf)
+Array.prototype.findDuplicate = function() {
+
+    let s = new Set()
+    let deplicates= [ ]
+     for(let val of this) { 
+         if(!s.has(val)) {
+            s.add(val)  
+         }else{
+            deplicate.push(val)
+         }
+     }
+      
+     return {
+       deplicates , unique: Array.from(s)
+     }
+
+}  // prototype for findduplicates and unique 
+ 
+
+ 
+let arr =[3,6,7,8,9,90,3,4,54,55 , 6 , ]
+
+ console.log(arr.findDuplicate())
+ console.log(arr.removeDupulicte())
+
+
+
+
+
+//   function fcf() {
+//       console.log("callback ")
+// }
+
+ 
+// function root(fcf) {
+//      console.log("some asynchronous work here ")
+//       fcf()     
+//  }
+
+ 
+// root(fcf)
