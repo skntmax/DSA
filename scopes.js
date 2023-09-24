a42abc589c3575169ab2d976847fa6284993d359
Shashi Kant <skntjee@gmail.com>
2023-09-24 13:18:42 +0530

    checkout

---
+++ scopes.js
@@ -0,0 +1,18 @@
+// 1
+
+
+
+var name ="sknt"
+ 
+function functionName(){
+    console.log(name) //  if variable is not present in loval scope then it search in global scope     
+    const temp = ()=>{
+        var name = "idk"
+     }   
+     temp()
+}
+
+functionName()
+
+
+
