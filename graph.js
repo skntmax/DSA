1bc9c4677f1c59473af726c30e11375f85870bfa
Shashi Kant <skntjee@gmail.com>
2023-10-02 00:37:27 +0530

    added stations , remove stations , suggested paths

--- graph.js
+++ graph.js
@@ -10,7 +10,7 @@ class graph  {
         }
         
       this.stations = { }
-      this.all_suggestedPaths=[]
+      this.suggested_paths =  new Set() 
 
      }
 
@@ -19,80 +19,104 @@ class graph  {
             if(Object.keys(this.stations).length==0) {
                 for(let val of this.nodes ) {
                     this.stations[val] = []
-                  }
+                   }
             }
           
         for(let nodes in this.stations) {
-           if(nodes==s) {
+           if(nodes==s && !this.stations[nodes].includes(d) ) {
              this.stations[s] = [...this.stations[nodes] , d ]
             } 
         }
          return this.stations
      }
 
+
+      
      addStation(station) {
        this.nodes.push(station)
      }
 
+      
      removeStations(station) {
         for(let st in this.stations) {  
           if(this.stations[st].includes(station)) {
              this.stations[st] = this.stations[st].filter(ele=> ele!= station )
           }
           delete this.stations[station]
+          this.nodes = this.nodes.filter(ele=> ele!=station )
         }
     return this.stations 
     }
-      
 
-    getSubPath(current_iteration_key , src ,  dst , sub_list ){
-      // sub_list =  B: [ 'D', 'A' ],
-      // src= A
-      // dst = D
-      // current_iteration =B
-       let edge = []
-       let path = []
 
-        
-          if(sub_list.includes(dst)){
-                let indexOfStation =  sub_list.indexOf(dst)
-                edge.push(indexOfStation)
-                path.push( current_iteration_key )
-                return src+path.join('')+dst
-            } else{
-               return ""
-            }
+     
+  
+
+    actualPath (current_key , s, d) {
+      let path = []
+       
+       return ()=>{
+        let it_arr = this.stations[current_key]
+        for(let i=0; i<it_arr.length ; i++) {
+           if(it_arr[i]==d) {
+             path.push(s+current_key+d) 
+           }else if(it_arr[i]!=s) {
+              this.actualPath( it_arr[i] , s , d)            
+            }   
         }
+        
+         if(path.length>0)
+            return path
+          else 
+          return "" 
+         
+       }
+      
+    
+    }
+  
+ 
 
-
-     suggestedPath (src, dst ) {
+     
+    
+    getSuggestePath(s,d) {
+        // s=A, d=D
+          
+            let valid_key_source = Object.keys(this.stations).indexOf(s)
+            let valid_key_dst = Object.keys(this.stations).indexOf(d)
              
-      for(let [ key , value ] of Object.entries(this.stations) ) {
-
-            if(  this.stations[key].includes(dst) ) {
-                 let  i=0
-                   while(i<this.stations[key].length){
-                     let path = this.getSubPath(key , src ,  dst , this.stations[key] )     
-                     if(path){
-                       this.all_suggestedPaths.push(path)
-                     }
-                     i++
-                   }
+             if( valid_key_source>-1  && valid_key_dst>-1   ) {
+                
+              
+              for(let key in this.stations ) {
+                 if(key!=d) {
+                       let i=0
+                       while(i<this.stations[key].length) {
+                           let final_path =   this.actualPath(key ,s, d )()                       
+                           if(final_path.length>0)this.suggested_paths.add(final_path[0])
+                          i++
+                          }     
+                   
+                      }  
+               }
+   
+             }else{
+               return "not valid stations"
              }
-
-      }       
-      }
-
+    
+     }
+     
      
  } 
 
   
- let trainA =new graph ( ['A' ,'B' ,"C" ,"D" ,"E"]   )
+ let trainA =new graph ( ['A' ,'B' ,"C" ,"D" ,"E" ,"F" ,"G" ,"H"]   )
  
-
+  
  trainA.addSubstations("A" , "B" )
  trainA.addSubstations("A" , "C" )
  trainA.addSubstations("C" , "A" )
+ trainA.addSubstations("C" , "D" )
  trainA.addSubstations("B" , "D" )
  trainA.addSubstations("B" , "A" )
  trainA.addSubstations("D" , "B" )
@@ -101,10 +125,13 @@ class graph  {
  trainA.addSubstations("E" , "D" )
  trainA.addSubstations("A" , "E" )
  trainA.addSubstations("E" , "B" )
- trainA.suggestedPath("A" , "D" )
-  
- //  trainA.removeStations('C')
+ trainA.addSubstations("F" , "D" )
+ trainA.addSubstations("H" , "D" )
+ trainA.addSubstations("G" , "H" )
+ trainA.addSubstations("H" , "A" )
 
-
-  
+//  trainA.removeStations('A')
+ trainA.getSuggestePath('A' ,"D" )
+   
   console.log("trainA>>>" , trainA); 
+   
