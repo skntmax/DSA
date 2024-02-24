835582dafe0d5ad283fc59e6388bc72416ae1abc
Shashi Kant <skntjee@gmail.com>
2024-02-24 18:19:39 +0530

    flaten the array

---
+++ FlattenArray.js
@@ -0,0 +1,26 @@
+let array = [[234, 56, 788, 23], [333], [23, 45], [45, 7], 0];
+
+function flattenArray(arr, depth = 1) {
+  let res = [];
+  if (arr.length == 0) return [];
+  if (depth == 0) return res;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (depth >= 1) {
+      depth--;
+      flattenArray(arr[i], depth);
+    }
+
+    if (Array.isArray(arr[i]) && depth == 0) {
+      arr[i].forEach((element) => {
+        res.push(element);
+      });
+    } else {
+      res.push(arr[i]);
+    }
+  }
+
+  return res;
+}
+
+console.log(flattenArray(array, 1));
