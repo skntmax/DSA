a6e484704cb7cd0b9f36d14c297831d962aae0fe
shashi kant <skntjee@gmail.com>
2026-02-12 21:01:21 +0530

    added advance node js

--- recursion/subset_string.js
+++ recursion/subset_string.js
@@ -1,19 +1,33 @@
 let str = "abc";
 
-function subset(str, i, res, output) {
-  if (i === str.length) {
-    output.push(res);
-    return; 
+// function subset(str, i, res, output) {
+//   if (i === str.length) {
+//     output.push(res);
+//     return; 
+//   }
+
+// //   console.log("take it>> ",i,res + str[i]);
+// //   console.log("exclude>>", i, res);
+//   subset(str, i + 1, res + str[i], output); // include 
+
+//   subset(str, i + 1, res, output); // don't include 
+// }
+
+// let result = [];
+// subset(str, 0, "", result);
+
+// console.log(result)
+
+function subset(p , up ){
+  if(up==""){
+    console.log(p);
+    return;
   }
 
-//   console.log("take it>> ",i,res + str[i]);
-//   console.log("exclude>>", i, res);
-  subset(str, i + 1, res + str[i], output); // include 
+  let ch =  up[0]
+  subset(p+ch , up.slice(1) )
+  subset(p , up.slice(1) )
 
-  subset(str, i + 1, res, output); // don't include 
-}
+} 
 
-let result = [];
-subset(str, 0, "", result);
-
-console.log(result)
+subset("" , str)
